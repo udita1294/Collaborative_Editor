e6c743022d5f503bfe718c4258b2bb41f07a17dc
Udita Singh <udita1294@gmail.com>
2026-03-29 11:59:27 +0530

    Editor is created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Editor } from '@monaco-editor/react'
 
 const App = () => {
   return (
-    <div>
+    <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
+
+      <aside className='h-full w-1/4 bg-amber-50 rounded-lg'></aside>
+
+      <section className='w-3/4 overflow-hidden bg-neutral-800 rounded-lg pt-0.5'>
+        <Editor
+          height="100%"
+          language="javascript"
+          theme="vs-dark"
+          defaultValue="// Write your code here..."
+        />
+      </section>
+
       
-    </div>
+    </main>
   )
 }
 
